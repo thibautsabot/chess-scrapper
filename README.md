37e0fe864a0bcdb2fa1ff587ad889915a8885130
Thibaut Sabot <thibaut.sabot@adevinta.com>
2021-02-17 23:26:36 +0100

    add todo

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 `node index.js`
 
 Requirement : Node 14+
+
+TODO :
+
+- check for `undefined` and `id` URLs
+- create folders (and check if they exist)
+- Download videos in each folder by using the lesson name and the video URL
--- chapters.js
+++ chapters.js
@@ -4,22 +4,23 @@ const getChapters = async () => {
   const chapters = (
     await getHtmlFromUrl("https://www.chess.com/lessons")
   ).querySelectorAll(".course-component");
-  
-  if (!chapters) return
+
+  if (!chapters) return;
 
   return chapters.map((chapter) => {
     const chapterTitleElement = chapter.querySelector(".course-title");
-    const chapterAttrs = chapter?.rawAttrs
-  
+    const chapterTitle = sanitizeText(
+      chapterTitleElement.childNodes[0].rawText
+    );
+
+    const chapterAttrs = chapter?.rawAttrs;
     const chapterLink = chapterAttrs
       .substring(0, chapterAttrs.indexOf("\n"))
       .replace(/href=/, "")
       .replace(/"/g, "");
-  
-    const chapterTitle = sanitizeText(chapterTitleElement.childNodes[0].rawText);
-  
-    return { title: chapterTitle, url: chapterLink }
-  });
-}
 
-export { getChapters }
+    return { title: chapterTitle, url: chapterLink };
+  });
+};
+
+export { getChapters };
